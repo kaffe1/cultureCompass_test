fc520ec3ad3bd81ae30a9e131cf632ab57979072
kaffe <jintongj@kth.se>
2026-02-25 10:24:27 +0100

    add workflows

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
+
 export default defineConfig({
+  base: "/cultureCompass_test/", // 例如你的仓库叫 'my-project'，这里就写 '/my-project/'
   plugins: [react()],
-})
+});
